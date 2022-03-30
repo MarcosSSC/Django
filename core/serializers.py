081e1aeca77307c8dad50901ecb5bd0c524a0501
marcos <marcos_ssc@hotmail.com>
2022-03-30 14:15:23 -0400

    Commit das aulas 4, 5 e 6

--- core/serializers.py
+++ core/serializers.py
@@ -7,7 +7,22 @@ class ZoneSerializer(serializers.ModelSerializer):
         model = models.Zone
         fields = '__all__'
 
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Department
-        fields = '__all__'
+#class DepartmentSerializer(serializers.ModelSerializer):
+#    class Meta:
+ #       model = models.Department
+ #       fields = '__all__'
+
+class DepartmentSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    created_at =  serializers.DateTimeField(required=False)
+    modified_at = serializers.DateTimeField(required=False)
+    active = serializers.BooleanField(required=False)
+    name = serializers.CharField(required=False, max_length=64)
+
+    def validate(self, attrs):
+        if not attrs.get('name').isupper():
+            raise Exception('O NOME DO DEPARTAMENTO DEVE SER EM MAIÚSCULO ')
+        return super(DepartmentSerializer, self).validate(attrs)
+
+    def create(self, validated_data):
+        return models.Department.objects.create(**validated_data)
--- sale/settings.py
+++ sale/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_methods
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'sale.urls'
@@ -135,3 +137,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter'
     )
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = default_methods
